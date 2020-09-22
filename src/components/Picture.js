372efab61d1b98efb0f36ef84e0b3c3b6f59dbb7
himanshu tripathi <sunny.era@gmail.com>
2020-09-22 16:58:38 +0200

    like ,cart buttons

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Picture({ picture }) {
+  const [hovered, setHovered] = useState(false);
+
+  const likeIcon = hovered && <span className="like">like </span>;
+  const cartIcon = hovered && <span className="cart">cart </span>;
+
   return (
-    <div className="image-container">
+    <div
+      className="image-container"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
       <img src={picture.thumbnailUrl} className="" alt="empty space" />
+      {likeIcon} {cartIcon}
     </div>
   );
 }
