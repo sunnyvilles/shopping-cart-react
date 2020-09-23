86124e619cde5e85c23cc2504e17c399a98cb14c
himanshu tripathi <sunny.era@gmail.com>
2020-09-23 16:25:34 +0200

    fill cart in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { appContext } from "../appContext.js";
 
 function Header() {
+  const { cartItems } = useContext(appContext);
+
+  const cartIconFilled =
+    cartItems.length > 0 ? "ri-shopping-cart-fill" : "ri-shopping-cart-line";
+
   return (
     <header>
       <Link to="/">
         <h1>Pictures</h1>
       </Link>
-      <Link to="/cart">Cart</Link>
+      <Link to="/cart">
+        <i className={`${cartIconFilled} ri-fw ri-2x`}></i>
+      </Link>
     </header>
   );
 }
